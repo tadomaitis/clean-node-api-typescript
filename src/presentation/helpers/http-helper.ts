9c476508da3fab84e0af62369df0cde89d35d582
Thiago Adomaitis <t.d.adomaitis@gmail.com>
2020-09-22 06:26:36 -0300

    refactor: move success httpResponse to a helper method

--- src/presentation/helpers/http-helper.ts
+++ src/presentation/helpers/http-helper.ts
@@ -10,3 +10,8 @@ export const serverError = (): HttpReponse => ({
   statusCode: 500,
   body: new ServerError()
 })
+
+export const ok = (data: any): HttpReponse => ({
+  statusCode: 200,
+  body: data
+})
