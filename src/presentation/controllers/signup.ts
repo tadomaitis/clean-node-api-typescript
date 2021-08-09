759750744c560be99cb9f5baa8f961a6b64f765d
Thiago Adomaitis <t.d.adomaitis@gmail.com>
2021-08-08 21:39:50 -0300

    fix: fix typo on file name and its importations

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,5 +1,5 @@
 import { HttpResponse, HttpRequest } from '../protocols/http'
-import { MissingParamError } from '../errors/missing-param-errors'
+import { MissingParamError } from '../errors/missing-param-error'
 import { badRequest } from '../helpers/http-helper'
 import { Controller } from '../protocols/controller'
 
