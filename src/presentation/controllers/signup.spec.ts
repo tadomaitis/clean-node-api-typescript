83f9a57df2cc12928406735fb84bf71730d7e13b
Thiago Adomaitis <t.d.adomaitis@gmail.com>
2021-08-08 21:23:52 -0300

    refactor: remove duplicated test

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -43,19 +43,6 @@ describe('SignUp Controller', () => {
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new MissingParamError('password'))
   })
-  test('Should return 400 if no password is provided', () => {
-    const sut = new SignUpController()
-    const httpRequest = {
-      body: {
-        name: 'any_name',
-        email: 'any_email@mail.com',
-        passwordConfirmation: 'any_password'
-      }
-    }
-    const httpResponse = sut.handle(httpRequest)
-    expect(httpResponse.statusCode).toBe(400)
-    expect(httpResponse.body).toEqual(new MissingParamError('password'))
-  })
   test('Should return 400 if no password confirmation is provided', () => {
     const sut = new SignUpController()
     const httpRequest = {
