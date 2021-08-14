11811c80e010be1ad5e9f62c0c7bf863cdc9f0f0
Thiago Adomaitis <t.d.adomaitis@gmail.com>
2021-08-14 17:42:59 -0300

    refactor: edit value on test to be more semantic

--- src/presentation/controllers/signup/signup.spec.ts
+++ src/presentation/controllers/signup/signup.spec.ts
@@ -17,7 +17,7 @@ const makeAddAccount = (): AddAccount => {
       const fakeAccount = {
         id: 'valid_id',
         name: 'valid_name',
-        email: 'valid_mail@mail.com',
+        email: 'valid_email@mail.com',
         password: 'valid_password'
       }
       return await new Promise(resolve => resolve(fakeAccount))
@@ -212,7 +212,7 @@ describe('SignUp Controller', () => {
     expect(httpResponse.body).toEqual({
       id: 'valid_id',
       name: 'valid_name',
-      email: 'valid_mail@mail.com',
+      email: 'valid_email@mail.com',
       password: 'valid_password'
     })
   })
