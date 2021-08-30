daf4f070cae1086f7f3031e7c893ed3f6344ec17
Thiago Adomaitis <t.d.adomaitis@gmail.com>
2021-08-30 20:49:11 -0300

    test: ensure LogMongoRepository creates an error log on success

---
+++ src/infra/db/mongodb/log-repository/log.spec.ts
@@ -0,0 +1,27 @@
+import { LogMongoRepository } from './log'
+import { MongoHelper } from '@/infra/db/mongodb/helpers/mongo-helper'
+import { Collection } from 'mongodb'
+
+describe('Log Mongo Repository', () => {
+  let errorsCollection: Collection
+
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    errorsCollection = await MongoHelper.getCollection('errors')
+    await errorsCollection.deleteMany({})
+  })
+
+  test('Should create an error log on success', async () => {
+    const sut = new LogMongoRepository()
+    await sut.logError('any_error')
+    const count = await errorsCollection.countDocuments()
+    expect(count).toBe(1)
+  })
+})
